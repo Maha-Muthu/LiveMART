4f2e7f0f715e478ea817b87c8421a60cfb6483f0
unknown <mahalaxmi.muthuvel@gmail.com>
2021-04-17 04:31:09 +0530

    WholeSaler Add Delete Items

--- OnlineDelivery/app.py
+++ OnlineDelivery/app.py
@@ -19,8 +19,8 @@ def logged():
             if(role=="Wholesaler"):
                 cur.execute("SELECT * FROM  Wholesalers WHERE username=?", (uname,))
                 rows=cur.fetchall()
-                if rows[0][1]==password:
-                    session['username']=request.form['Username']
+                if rows[0][2]==password:
+                    session['Id']=rows[0][0]
                     return redirect(url_for('wholesalerHome'))
                 else:
                     flash('Invalid Credentials... Try Again !!')
@@ -32,13 +32,13 @@ def logged():
 
 @app.route('/wholesalerHome')
 def wholesalerHome():
-    val =session.get('username', None)
+    val =session.get('Id', None)
     connection = sqlite3.connect("database.db")
     return render_template("WholesalerHome.html")
 
 @app.route('/wholesalerAddItem',methods=['POST','GET'])
 def wholesalerAddItem():
-    val =session.get('username', None)
+    val =session.get('Id', None)
     connection = sqlite3.connect("database.db")
     return render_template("WholesalerUpload.html")
 
@@ -47,6 +47,7 @@ def wholesalerAddItem():
 def wholesalerUpdateItem():
     if request.method == 'POST':
         try:
+            val =session.get('Id', None)
             itemCategory = request.form['itemCategory']
             itemId = request.form['itemId']
             itemName = request.form['itemName']
@@ -54,9 +55,36 @@ def wholesalerUpdateItem():
             itemQuantity = request.form['itemQuantity']
             with sqlite3.connect("Database.db") as connection:
                 cur = connection.cursor()
-                cur.execute("INSERT INTO Items (ItemId,ItemName,ItemPrice,ItemQuantity,ItemCategory) VALUES (?,?,?,?,?);", (itemId,itemName,itemPrice,itemQuantity,itemCategory))
+                cur.execute("INSERT INTO Items (ItemId,ItemName,ItemPrice,ItemQuantity,ItemCategory,ItemOwnerId) VALUES (?,?,?,?,?,?);", (itemId,itemName,itemPrice,itemQuantity,itemCategory,val))
                 connection.commit()
                 return "Inserted"
+        except:
+            connection.rollback()
+            return "Failed"
+        finally:
+            connection.close()
+    return "EXIT"
+
+@app.route('/wholesalerDeleteItem',methods=['POST','GET'])
+def wholesalerDeleteItem():
+    curid =session.get('Id', None)
+    connection = sqlite3.connect("database.db")
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM Items WHERE ItemOwnerId=?", (curid,))
+    rows = cur.fetchall()
+    return render_template("WholesalerRemove.html",rows=rows)
+
+
+@app.route('/wholesalerRemoveItem',methods=['POST','GET'])
+def wholesalerRemoveItem():
+    if request.method == 'POST':
+        try:
+            itemId = request.form['itemId']
+            with sqlite3.connect("Database.db") as connection:
+                cur = connection.cursor()
+                cur.execute("DELETE FROM Items  WHERE ItemId=?", (itemId,))
+                connection.commit()
+                return "Deleted"
         except:
             connection.rollback()
             return "Failed"
